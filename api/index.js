409c8118e2b22e95c99dee1af19f6f569cd371f7
ashishEV114 <123160767+ashishEV114@users.noreply.github.com>
2023-08-18 20:08:13 +0530

    added Mongo DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log("Connected to database");
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
-app.listen('3000', () => {
-    console.log('Server is listening on port 3000!');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
 })
