fa78a9b99c18d2d97cbc7b3b1b48f7163b1c3396
ashishEV114 <123160767+ashishEV114@users.noreply.github.com>
2023-08-18 23:21:54 +0530

    API library

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import createError from "http-errors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 mongoose.connect(process.env.MONGODB).then(() => {
     console.log("Connected to database");
@@ -10,6 +14,26 @@ mongoose.connect(process.env.MONGODB).then(() => {
 })
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use('/', (req, res, next) => {
+    next(createError.NotFound());
+})
+
+// Error Handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
---
+++ api/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+import { test } from "../controllers/user.controller.js";
+
+router.get('/', test);
+
+export default router;
