99c9c2ead2adb0548b723b2848b54e5a052c30d8
ashishEV114 <123160767+ashishEV114@users.noreply.github.com>
2023-08-19 08:29:49 +0530

    Error middle ware and user entry api

--- api/index.js
+++ api/index.js
@@ -29,6 +29,7 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.send({
         error: {
+            success: false,
             status: err.status || 500,
             message: err.message
         }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { signUp } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/signup');
+router.post('/signup', signUp);
 
 export default router;
